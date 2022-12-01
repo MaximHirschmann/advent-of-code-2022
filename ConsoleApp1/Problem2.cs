057d7d4b610ab1b543ae73c0a549923d0b5ef1d5
Maxim <maxim.hirschmann@campus.tu-berlin.de>
2022-12-01 12:21:26 +0100

    day 1

---
+++ ConsoleApp1/Problem2.cs
@@ -0,0 +1,20 @@
+﻿class Problem2
+{
+    public static void Solve()
+    {
+        string[] input = Solution.ReadInput();
+
+        List<int> elfs = Solution.Aggregate(input);
+
+        // O(n)
+        int res = 0;
+        for (int i = 0; i < 3; i++)
+        {
+            int maxVal = elfs.Max();
+            elfs.Remove(maxVal);
+            res += maxVal;
+        }
+
+        Console.WriteLine(res);
+    }
+}
