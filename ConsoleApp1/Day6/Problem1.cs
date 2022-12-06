3276382c09ec389e70111f4a287005afaf5fad49
MaximHirschmann <maximfedorov978@gmail.com>
2022-12-06 10:15:45 +0100

    Day 6

---
+++ ConsoleApp1/Day6/Problem1.cs
@@ -0,0 +1,27 @@
+﻿namespace Day6
+{
+    class Problem1
+    {
+        public static void Solve()
+        {
+            string[] input = Solution.ReadInput();
+
+            Queue<char> queue = new();
+            for (int i = 0; i < input[0].Length; i++)
+            {
+                char c = input[0][i];
+                queue.Enqueue(c);
+                if (queue.Count() > 4)
+                {
+                    queue.Dequeue();
+
+                    if (queue.Distinct().Count() == 4)
+                    {
+                        Console.WriteLine(i+1);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Day6/Problem2.cs
@@ -0,0 +1,27 @@
+﻿namespace Day6
+{
+    class Problem2
+    {
+        public static void Solve()
+        {
+            string[] input = Solution.ReadInput();
+
+            Queue<char> queue = new();
+            for (int i = 0; i < input[0].Length; i++)
+            {
+                char c = input[0][i];
+                queue.Enqueue(c);
+                if (queue.Count() > 14)
+                {
+                    queue.Dequeue();
+
+                    if (queue.Distinct().Count() == 14)
+                    {
+                        Console.WriteLine(i + 1);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
